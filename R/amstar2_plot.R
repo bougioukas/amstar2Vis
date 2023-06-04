079e9b9f1f7b324a4c01b307fbc9d8a28bb39f09
bougioukas <mpougioukas@auth.gr>
2023-06-04 22:17:30 +0300

    1.01

---
+++ R/amstar2_plot.R
@@ -0,0 +1,66 @@
+#' @title Plots the AMSTAR plot
+#'
+#' @description This function plots the AMSTAR bar plot.
+#'
+#' @param data A dataset.
+#'
+#' @return amstar2_plot
+#'
+#' @export
+#'
+#' @examples
+
+
+amstar2_plot <- function(data = data){
+
+  data <- data[, -1]
+  amstar2_items <- stats::na.omit(data)
+
+  label <- c("1. PICO components","2. Preestablished protocol","3. Explanation of inlcuded studies' design",
+             "4. Comprehensive search strategy","5. Duplicate study selection","6. Duplicate data extraction",
+             "7. List of excluded studies and justification","8. Description of included studies",
+             "9. Risk of bias (RoB) assessment","10. Funding sources", "11. Use of appropriate statistical methods",
+             "12. RoB impact on synthesized results", "13. Results interpretation with RoB reference",
+             "14. Heterogeneity explanation", "15. Publication/ small study bias investigation",
+             "16. Conflict of interest declaration")
+
+  names(amstar2_items) <- label
+
+  amstar2_items_long <- tidyr::pivot_longer(amstar2_items, cols = 1:16, names_to = "item", values_to = "assessment")
+
+  amstar2_items_long$item <- factor(amstar2_items_long$item, levels= label)
+
+  amstar2_items_long$assessment <- factor(amstar2_items_long$assessment,
+                                     levels = c("Yes", "Partial Yes", "No", "Not Applicable"))
+
+
+  amstar2_barplot <- amstar2_items_long |>
+    dplyr::group_by(item, assessment) |>
+    dplyr::summarise(n = dplyr::n()) |>
+    dplyr::mutate(prop = n/sum(n)) |>
+    dplyr::ungroup()
+
+
+
+amstar_barplot <- ggplot2::ggplot(amstar2_barplot, ggplot2::aes(x = prop, y = forcats::fct_rev(item), fill = forcats::fct_rev(assessment))) +
+  ggplot2::geom_col(position = "fill", width = 0.8) +
+  ggplot2::geom_text(ggplot2::aes(label = paste0(round(prop, digits = 3)*100,"%")), size = 3.5,
+              color="white",
+              position = ggplot2::position_stack(vjust = 0.5)) +
+  ggplot2::labs(x = paste0("Percentage of SRs (%), N=",  nrow(amstar2_items)),
+         y = "Items of AMSTAR 2 checklist") +
+  ggplot2::scale_fill_manual(values = c("#999999" , "#ffcf20FF",  "#20908C", "#3a5e8cFF")) +
+  ggplot2::guides(fill = ggplot2::guide_legend(reverse = TRUE, title = "Rating")) +
+  ggplot2::scale_x_continuous(labels = scales::percent, n.breaks = 10) +
+  ggplot2::theme(panel.grid.minor = ggplot2::element_blank(),
+          panel.background = ggplot2::element_blank(),
+          axis.text = ggplot2::element_text(size=12),
+          axis.title.x = ggplot2::element_text(size = 14, vjust = -0.75),
+          axis.title.y = ggplot2::element_text(size = 14, vjust = +0.8),
+          legend.position = "bottom",
+          legend.text = ggplot2::element_text(size = 12),
+          legend.title = ggplot2::element_text(size = 12, face = "bold", margin = ggplot2::margin(0,50,0,0)),
+          legend.key.size = grid::unit(0.6, "cm"))
+
+  return(amstar_barplot)
+}
